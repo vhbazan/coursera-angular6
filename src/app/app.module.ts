440078a55de27a118d1403c857a1499fb6eb3f69
Victor Bazan <vhbazanm@gmail.com>
2018-08-22 00:07:56 +0100

    Angular Assignment 1: Angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,13 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishDetailComponent
   ],
   imports: [
     BrowserModule, 
