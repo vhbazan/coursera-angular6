d0d60ef93aa9467af21607c553ca665ae2c98a6f
Victor Bazan <vhbazanm@gmail.com>
2018-08-25 00:16:26 +0100

    Add Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +35,7 @@ import { DishDetailComponent } from './dish-detail/dish-detail.component';
     MatListModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
